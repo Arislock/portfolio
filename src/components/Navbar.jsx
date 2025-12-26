528bc4aef9570e363745300bc7f5ddda46e083e0
Arislock <melodywang2244@gmail.com>
2025-12-26 12:41:30 -0800

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return (<nav className="fixed top-0 w-full z-40">
+     <div className="max-w-5xl mx-mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+            <a href="#home">home</a>
+            <a href="#home">projects</a>
+            <a href="#home">about</a>
+            <a href="#home">contact</a>
+
+        </div>
+     </div>
+    </nav>
+    )
+}
